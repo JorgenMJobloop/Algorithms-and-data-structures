eb87d6edc60ba70555b9a892f7170569310daec0
JorgenMJobloop <jorgen@jobloop.no>
2024-09-12 11:09:51 +0200

    Update Makefile.mk

--- Makefile.mk
+++ Makefile.mk
@@ -8,12 +8,16 @@ all: c_program cpp_program
 
 c_program: quicksort_c.o
 	$(CC) $(CFLAGS) -o c_program quicksort_c.o
+	$(CC) $(CFLAGS) -o c_program mallocExample_c.o
 
 cpp_program: quicksort_cpp.o
 	$(CXX) $(CXXFLAGS) -o cpp_program quicksort_cpp.o
 
 quicksort_c.o: quicksort.c
 	$(CC) $(CFLAGS) -c quicksort.c
+
+mallocExample_c.o: mallocExample.c
+	$(CC) $(CFLAGS) -c mallocExample.c
 
 quicksort_cpp.o: quicksort.cpp
 	$(CXX) $(CXXFLAGS) -c quicksort.cpp

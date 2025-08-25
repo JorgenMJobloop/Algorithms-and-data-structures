87b8acaa8e59147215df20067422a93d05a0242c
JorgenMJobloop <jorgen@jobloop.no>
2025-08-25 13:03:41 +0200

    Worked on the binary search implementation in C

--- BinarySearch.c
+++ BinarySearch.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
+
+const int MAX_SIZE = INT_MAX;
+
+// This struct acts as a "generic" built-in length type, usually seen in higher level languages like Java, C#, JS etc
+typedef struct
+{
+    int arr[150]; // Max len 150
+    size_t len;
+} ArrayLength;
 
 int binary_search(int arr[], int x)
 {
     int left = 0;
-    int length = 10;
+    const int length = 10; // This might not have to be a const, as we can use the ArrayLength struct.
     int right = length - 1;
 
     while (left <= right)
@@ -28,18 +38,57 @@ int binary_search(int arr[], int x)
     return -1;
 }
 
+/// @brief
+/// @param arr int array of size MAX_LENGTH
+/// @param length length of a given array of size MAX_LENGTH where MAX_LENGTH = MAX_INT
+void calculate_length(int arr[], size_t length)
+{
+    if (length == MAX_SIZE)
+    {
+        fprintf("The length of the array exceeded the maximum allowed size!\nTerminating program.", stdout);
+        return -1;
+    }
+    printf("Length of the array: %zu\n", length);
+}
+
+void test_implementation()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    printf("Input the number you wish to search for: \n");
+    int x;
+    int val = scanf("%d", &x);
+    printf("Value of x currently: %d\n", x);
+    int output = binary_search(arr, x);
+    printf("Output: %d\n", output);
+    if (output == -1)
+    {
+        printf("The algorithm did not locate the value of x. And thus failed!\n");
+    }
+    else
+    {
+        printf("The algorithm found x at: %d at position: %d\n", x, arr[x]);
+    }
+
+    printf("Verifying if the length is properly calculated!\n");
+    int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // len 10
+    size_t len = sizeof(array) / sizeof(array[0]);
+    calculate_length(array, len);
+}
+
 int main()
 {
     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int x = 5;
-    if (
-        binary_search(arr, x) == -1)
+
+    test_implementation();
+
+    if (binary_search(arr, x) == -1)
     {
         printf("The algorithm did not locate the x value. And thus failed!\n");
     }
     else
     {
-        printf("Algorithm found x");
+        printf("Algorithm found x at: %d\n", x);
     }
     return 0;
 }
